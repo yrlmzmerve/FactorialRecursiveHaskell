4c4af5ec98e1e2e3afc0d1b5110d57e34323c0ac
yrlmzmerve <vier-und.achtzig-tm@hotmail.com>
2018-05-20 15:00:29 +0300

    recursive function calculate factorial with HASKELL

---
+++ pllabhaskel.hs
@@ -0,0 +1,10 @@
+﻿factorial :: Int -> Int 
+factorial 0 = 1 
+factorial n = n * factorial ( n - 1 ) 
+
+main = do  
+
+    putStrLn "Enter number: "
+    contents <- getLine 
+    let number = read contents :: Int
+    print (factorial number) 
